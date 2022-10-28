5eabb29c778f83ff52b9a7313dbd1fb44b597796
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-10-29 00:43:53 +0100

    Add javascript interaction to mobile menu

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// Creating variables
+
+const menuBtn = document.querySelector('.header__navbar-menu');
+const closeBtn = document.querySelector('.header__navbar-close');
+const mobileMenu = document.querySelector('.header__navlinks');
+
+// Adding event listener to menu button
+menuBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('close');
+});
+
+// Adding event listener to close button
+closeBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('close');
+})
