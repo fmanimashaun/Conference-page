ee7a6cfc37e798e2255c3496d81216ad6193673b
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-10-29 09:23:26 +0100

    Update speaker section in index.html

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@
 const menuBtn = document.querySelector('.header__navbar-menu');
 const closeBtn = document.querySelector('.header__navbar-close');
 const mobileMenu = document.querySelector('.header__navlinks');
+const speakers = Array.from(document.querySelectorAll('.speaker__card'));
+const moreBtn = document.querySelector('.speakers__btn');
+
 
 // Adding event listener to menu button
 menuBtn.addEventListener('click', () => {
@@ -13,3 +16,16 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   mobileMenu.classList.toggle('close');
 });
+
+// add event listener to the more button
+moreBtn.addEventListener('click', () => {
+  speakers.forEach((speaker, index) => {
+    if (index > 0 && index !== 2) {
+      speaker.classList.toggle('close');
+    }
+  });
+
+  moreBtn.classList.add('close');
+});
+
+
