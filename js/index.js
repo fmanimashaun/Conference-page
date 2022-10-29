049388203869ae66a71d49795d232744bebaa3a4
Engr Animashaun Fisayo Michael <fisayo.animashaun@outlook.com>
2022-10-30 00:15:18 +0100

    Update index.js fie

--- js/index.js
+++ js/index.js
@@ -2,6 +2,58 @@
 const menuBtn = document.querySelector('.header__navbar-menu');
 const closeBtn = document.querySelector('.header__navbar-close');
 const mobileMenu = document.querySelector('.header__navlinks');
+const speakerContainer = document.querySelector('.speaker__cards');
+
+const speakers = [
+  {
+    id: 1,
+    status: 'shown',
+    image: 'img/speaker_01.png',
+    name: '',
+    title: '',
+    desc: '',
+  },
+  {
+    id: 1,
+    status: 'shown',
+    image: 'img/speaker_01.png',
+    name: '',
+    title: '',
+    desc: '',
+  },
+  {
+    id: 1,
+    status: 'shown',
+    image: 'img/speaker_01.png',
+    name: '',
+    title: '',
+    desc: '',
+  },
+  {
+    id: 1,
+    status: 'shown',
+    image: 'img/speaker_01.png',
+    name: '',
+    title: '',
+    desc: '',
+  },
+  {
+    id: 1,
+    status: 'shown',
+    image: 'img/speaker_01.png',
+    name: '',
+    title: '',
+    desc: '',
+  },
+  {
+    id: 1,
+    status: 'shown',
+    image: 'img/speaker_01.png',
+    name: '',
+    title: '',
+    desc: '',
+  }
+]
 
 // Adding event listener to menu button
 menuBtn.addEventListener('click', () => {
